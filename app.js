939a15eacfdf8c6c6eb2d0718707d89b8a29b91a
HamzaGorcevic <gorcevichamza76@gmail.com>
2023-01-19 00:45:22 +0100

    missing footer

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 let ul = document.querySelector("ul");
 let burger = document.querySelector(".burger");
-burger.addEventListener("click", () => {
-  if (ul.classList.value != "") {
-    ul.classList.remove("displayNone");
-    console.log(ul.classList, "In else");
-  } else {
-    ul.classList.add("displayNone");
-    console.log(ul.classList, "in if");
-  }
-});
+burger.addEventListener("click", toggler);
 
+function toggler(el) {}
 console.log(ul.classList, ul.classList.value == "displayNone", "muda");
+console.log(ul.innerHTML);
+const icons = document.querySelectorAll(".icon");
+icons.forEach((icon) => {
+  icon.addEventListener("click", (event) => {
+    icon.classList.toggle("open");
+    ul.classList.toggle("displayNone");
+    console.log(ul.classList);
+  });
+});
+if (window.screen.availWidth > 1299) {
+  ul.classList?.remove("displayNone");
+}
+
+console.log(window.screen);
